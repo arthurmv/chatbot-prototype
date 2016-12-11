64a4fe834e2d689f89c5a9cef79b82e6a985f2e4
Tim Lonsdale <timlonsdale@Tims-MBP.lan>
2016-12-11 02:11:32 +0000

    try sourcing from query

--- app.js
+++ app.js
@@ -16,9 +16,10 @@ function buildOpts( hostname ) {
 }
 
 function attachAPI( app ) {
-    app.post( "/api/sms", bodyParser.urlencoded({extended:true}), promiseHandler( api.sms ) );
-    app.post( "/api/web", bodyParser.json(), promiseHandler( api.web.new ) );
-    app.put( "/api/web/:id", bodyParser.json(), promiseHandler( api.web.receive ) );
+    app.use( bodyParser.json() );
+    app.post( "/api/sms", promiseHandler( api.sms ) );
+    app.post( "/api/web", promiseHandler( api.web.new ) );
+    app.put( "/api/web/:id", promiseHandler( api.web.receive ) );
 }
 
 // create a server that redirects naked domains to a www. prefix
